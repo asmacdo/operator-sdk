48f870014ce2593aac687ab3da8c466818b9bcf5
Austin Macdonald <austin@redhat.com>
2020-05-07 08:41:54 -0400

    use klakegg image to build site

--- hack/check-hugo-links.sh
+++ hack/check-hugo-links.sh
@@ -6,8 +6,9 @@ source ./hack/lib/common.sh
 
 header_text "Building html and checking links"
 
+klakegg/hugo:0.69.2-ext-ubuntu
+
 pushd website
 npm install postcss-cli autoprefixer
 hugo
-liche -d public -r -c 50 public
 popd
